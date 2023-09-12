eebbcf06d7745915692042bc959d02754c6a1894
Chris Fregly <dev@chrisfregly.com>
2023-09-12 14:09:37 -0700

    inital commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# boardgame
+
+This is a React component library for Quibbble boardgames. 
+
+## Build Dist
+
+```
+npm run build
+```
+
+## Start Dev Testing Env
+
+```
+npm run dev
+```
---
+++ vite.config.js
@@ -0,0 +1,19 @@
+//vite.config.js
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig ({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.jsx"),
+      name: "boardgame",
+      fileName: "index",
+    },
+    rollupOptions: {
+      external: ["react"],
+    },
+  },
+  server: {
+    open: './dev/index.html',
+  },
+});
